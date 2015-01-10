6f07742f1191c45f80ca2d1da9e79fbccec1947c
Samuel <samuel_perez@ucol.mx>
2015-01-09 22:48:00 -0600

    Retomando esto... o trueno el semestre

---
+++ Ultimo intento/Assets/Qualcomm Augmented Reality/Scripts/AugmentScale.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AugmentScale : MonoBehaviour {
+
+
+	public void scaleUp() {
+		
+		TrackableBehaviour[] trackables = FindObjectsOfType(typeof(TrackableBehaviour)) as TrackableBehaviour[];
+		
+		foreach (var tr in trackables) {
+			
+			if (tr.transform.childCount > 0) {
+				
+				Transform augmentation = tr.transform.GetChild(0);
+				Vector3 scale = augmentation.localScale;
+				augmentation.localScale = 1.5f * scale;
+			}
+		}
+	}
+}
