bf4b31f0ab19cd35689ec3f9138c95a93fb5d760
Samuel <samuel_perez@ucol.mx>
2015-01-30 13:42:38 -0600

    Prototipo 1 terminado

---
+++ Realidad Aumentada Chanal/Assets/Scripts/Buttons_controller.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Buttons_controller : MonoBehaviour {
+
+	public GameObject canva1;
+	public GameObject txt_1;
+
+	public GameObject canva2;
+	public GameObject txt_2;
+
+	public void console_swag(){
+		Debug.Log ("Swag");
+	}
+
+
+	public void change_canva1_adelante(){
+
+		Vector3 canv_pos = canva1.transform.position;
+		canv_pos.x += 2.0f;
+		canva1.transform.position = canv_pos;
+		Debug.Log ("Canva canbiada");
+
+
+	}
+	public void change_canva1_atras(){
+		
+		
+	}
+
+	public void change_canva2_adelante(){
+
+	}
+
+	public void change_canva2_atras(){
+		
+	}
+}
---
+++ Realidad Aumentada Chanal/Assets/Scripts/MarquerDos.cs
@@ -0,0 +1,100 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MarquerDos : MonoBehaviour , ITrackableEventHandler {
+	private TrackableBehaviour mTrackableBehaviour;
+	public GameObject globo_info;
+	public GameObject camara;
+	
+	#region UNTIY_MONOBEHAVIOUR_METHODS
+	
+	void Start()
+	{
+		mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+		if (mTrackableBehaviour)
+		{
+			mTrackableBehaviour.RegisterTrackableEventHandler(this);
+		}
+		globo_info.SetActive (false);
+	}
+	
+	void Update (){
+		globo_info.transform.LookAt (camara.transform.position);
+	}
+	
+	#endregion // UNTIY_MONOBEHAVIOUR_METHODS
+	
+	
+	
+	#region PUBLIC_METHODS
+	public void OnTrackableStateChanged(
+		TrackableBehaviour.Status previousStatus,
+		TrackableBehaviour.Status newStatus)
+	{
+		if (newStatus == TrackableBehaviour.Status.DETECTED ||
+		    newStatus == TrackableBehaviour.Status.TRACKED ||
+		    newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
+		{
+			OnTrackingFound();
+		}
+		else
+		{
+			OnTrackingLost();
+		}
+		Debug.Log ("Trakeando con swagtl");
+	}
+	
+	#endregion // PUBLIC_METHODS
+	
+	
+	
+	#region PRIVATE_METHODS
+	
+	
+	private void OnTrackingFound()
+	{
+		Renderer[] rendererComponents = GetComponentsInChildren<Renderer>(true);
+		Collider[] colliderComponents = GetComponentsInChildren<Collider>(true);
+		
+		// Enable rendering:
+		foreach (Renderer component in rendererComponents)
+		{
+			component.enabled = true;
+		}
+		
+		// Enable colliders:
+		foreach (Collider component in colliderComponents)
+		{
+			component.enabled = true;
+		}
+		
+		//Debug.Log("Trackable " + mTrackableBehaviour.TrackableName + " found con swag");
+		globo_info.SetActive (true);
+	}
+	
+	
+	private void OnTrackingLost()
+	{
+		Renderer[] rendererComponents = GetComponentsInChildren<Renderer>(true);
+		Collider[] colliderComponents = GetComponentsInChildren<Collider>(true);
+		
+		// Disable rendering:
+		foreach (Renderer component in rendererComponents)
+		{
+			component.enabled = false;
+		}
+		
+		// Disable colliders:
+		foreach (Collider component in colliderComponents)
+		{
+			component.enabled = false;
+		}
+		
+		//Debug.Log("Trackable " + mTrackableBehaviour.TrackableName + " lost sin swag");
+		
+		globo_info.SetActive (false);
+		
+	}
+	
+	#endregion // PRIVATE_METHODS
+}
